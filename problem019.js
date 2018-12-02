1fdd16142b5c891da50c61bd085dfbc847c2c61a
HappyArroW <stifmeister0700@gmail.com>
2018-12-03 00:21:40 +0100

    Update problem019.js

--- problem019.js
+++ problem019.js
@@ -1,9 +1,11 @@
-let _correct = 0;
-let _failed = 0;
-let _failedTests = [];
-const test = (id, test) => { if (test) { _correct++; } else { _failed++; _failedTests.push(id); }; };
-test(1, carry((a, b) => a - b)(2)(1) === 1);
-test(2, carry((a, b) => a * b)(2)(2) === 4);
-test(3, carry((a) => a - 1)(2) === 1);
-test(4, carry((a, b, c, d) => a + b + c +d)(2)(2)(2)(2) === 8);
-[_correct, _failed, _failedTests];
+function carry(a) {
+	var args = [];
+	var i = a.length;
+	return function(x) {
+		args.push(x);
+		if (i-- > 1) {
+			return arguments.callee;
+		}
+		return a.apply(this, args);
+	}
+}
